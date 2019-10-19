493ce37080b914f2cf334488a3f3614b82e6a4ce
gabyua <ugartegaby.@gmail.com>
2019-10-19 12:24:27 -0500

    Revert "Code for lcd added to the project"
    This reverts commit 64d56de1692cabe340b339a130c4db0e372e04d9.

--- DC_Motor_Controller/Debug/synergy/ssp/src/framework/tes/dave2d/dave_base.d
+++
@@ -1,5 +0,0 @@
-synergy/ssp/src/framework/tes/dave2d/dave_base.o: ../synergy/ssp/src/framework/tes/dave2d/dave_base.c \
-../synergy/ssp/src/framework/tes/dave2d/dave_base.h
-
-../synergy/ssp/src/framework/tes/dave2d/dave_base.h:
-
